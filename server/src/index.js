6192fb8de1cf00b7a3be82ec18d7a4a1ad7f5910
amarjupalli <ajupalli@turo.com>
2019-12-26 22:21:55 -0500

    add links for first query

--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,33 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
     type Query {
       info: String!
+      feed: [Link]!
+    }
+
+    type Link {
+      id: ID!
+      description: String!
+      url: String!
     }
 `;
 
+const links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "graphql tutorial"
+  }
+];
+
 const resolvers = {
   Query: {
-    info: () => "API for hackernews clone"
+    info: () => "API for hackernews clone",
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 };
 
