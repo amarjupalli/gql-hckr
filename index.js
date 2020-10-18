19768ae2a4864b30c00b37851106a66ad3a0200d
amarjupalli <ajupalli@turo.com>
2020-10-17 22:13:38 -0700

    feat: db connection setup and models

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const { ApolloServer } = require("apollo-server");
-
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config");
+const Post = require("./models/Post");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    testStr: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    testStr() {
-      return "testing string...";
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
     },
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
-});
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((error) => {
+    console.error("Server error");
+    console.log(error);
+  });
---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const { model, Schema } = require("mongoose");
+
+const postSchema = new Schema({
+  body: String,
+  username: String,
+  createdAt: String,
+  comments: [
+    {
+      body: String,
+      username: String,
+      createdAt: String,
+    },
+  ],
+  likes: [
+    {
+      username: String,
+      createdAt: String,
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+});
+
+const Post = model("Post", postSchema);
+
+module.exports = Post;
