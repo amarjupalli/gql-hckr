9a5c233b68afc4042ade428b559be90915e5c424
amarjupalli <ajupalli@turo.com>
2020-10-19 08:55:06 -0700

    feat: add reolvers to CRUD posts

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -5,11 +5,13 @@ const typeDefs = gql`
     id: ID!
     body: String!
     createdAt: String!
+    user: String!
     username: String!
   }
 
   type Query {
     getPosts: [Post]
+    getPost(postId: String!): Post!
   }
 
   input RegisterInput {
@@ -30,6 +32,8 @@ const typeDefs = gql`
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
+    addPost(body: String!): Post!
+    deletePost(postId: ID!): String!
   }
 `;
 
